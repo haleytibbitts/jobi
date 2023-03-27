eed5cd7e3ef0dd19bcc8871e14ae4b297e1efdb4
Haley Tibbitts <117307728+haleytibbitts@users.noreply.github.com>
2023-03-27 12:45:56 -0400

    added trendingJobs styling and carousel functionality

--- src/components/TrendingJobs.js
+++ src/components/TrendingJobs.js
@@ -10,6 +10,7 @@ import { useState } from "react";
 const TrendingJobs = () => {
   const [activeLeft, setActiveLeft] = useState(false);
   const [activeRight, setActiveRight] = useState(false);
+  const [carouselLocation, setCarouselLocation] = useState(0);
 
   const handleActiveLeft = () => {
     setActiveLeft(!activeLeft);
@@ -17,6 +18,20 @@ const TrendingJobs = () => {
 
   const handleActiveRight = () => {
     setActiveRight(!activeRight);
+  };
+
+  const handleCarouselLeft = () => {
+    if (carouselLocation < 0) {
+      const newLocation = carouselLocation + 280;
+      setCarouselLocation(newLocation);
+    }
+  };
+
+  const handleCarouselRight = () => {
+    if (carouselLocation !== -840) {
+      const newLocation = carouselLocation - 280;
+      setCarouselLocation(newLocation);
+    }
   };
 
   return (
@@ -29,6 +44,7 @@ const TrendingJobs = () => {
               className={activeLeft ? "active" : "inactive"}
               onPointerEnter={handleActiveLeft}
               onPointerLeave={handleActiveLeft}
+              onClick={handleCarouselLeft}
             >
               {activeLeft ? <BsArrowLeft /> : <BsArrowLeftShort />}
             </button>
@@ -36,76 +52,99 @@ const TrendingJobs = () => {
               className={activeRight ? "active" : "inactive"}
               onPointerEnter={handleActiveRight}
               onPointerLeave={handleActiveRight}
+              onClick={handleCarouselRight}
             >
               {activeRight ? <BsArrowRight /> : <BsArrowRightShort />}
             </button>
           </div>
         </div>
-        <ul className="jobs">
-          <li>
-            <Link to={"/jobs/1"}>
-              <img
-                src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8"
-                alt="A macbook on a table displaying a WordPress website."
-              />
-              <p>WordPress Development</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"/jobs/2"}>
-              <img
-                src="https://images.unsplash.com/photo-1528109901743-12b16e05eedf"
-                alt="A close up of someone editing a video on a MacBook."
-              />
-              <p>Audio & Video Editing</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"jobs/3"}>
-              <img
-                src="https://images.unsplash.com/photo-1574852859542-1b41217a7815"
-                alt="A person with headphones on drawing a product design."
-              />
-              <p>Product & Branding Design</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"jobs/4"}>
-              <img
-                src="https://images.unsplash.com/photo-1632406898177-95f7acd8854f"
-                alt="A person sitting with their notebook open and their monthly budget open on their MacBook"
-              />
-              <p>Admin & Customer Support</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"jobs/5"}>
-              <img
-                src="https://images.unsplash.com/photo-1521898284481-a5ec348cb555"
-                alt="A person with headphones on sitting on chair and working at their MacBook"
-              />
-              <p>Quality Assurance Analysis</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"jobs/6"}>
-              <img
-                src="https://images.unsplash.com/photo-1520333789090-1afc82db536a"
-                alt="A person with glasses sitting at their computer and looking at their smartphone"
-              />
-              <p>Social Media Management</p>
-            </Link>
-          </li>
-          <li>
-            <Link to={"jobs/6"}>
-              <img
-                src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e"
-                alt="A person sitting at a table writing on notebook. On the table is a stack of books, a pile of polaroids, a MacBook, an iPad, a pair of glasses, and a cup of black coffee."
-              />
-              <p>Copywriting</p>
-            </Link>
-          </li>
-        </ul>
+        <div className="carousel">
+          <ul className="jobs" style={{ left: `${carouselLocation}px` }}>
+            <li>
+              <Link to={"/jobs/1"}>
+                <img
+                  src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8"
+                  alt="A macbook on a table displaying a WordPress website."
+                />
+                <div className="overlay">
+                  <p>WordPress Development</p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/jobs/2"}>
+                <img
+                  src="https://images.unsplash.com/photo-1528109901743-12b16e05eedf"
+                  alt="A close up of someone editing a video on a MacBook."
+                />
+                <div className="overlay">
+                  <p>
+                    Audio & <span>Video Editing</span>
+                  </p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"jobs/3"}>
+                <img
+                  src="https://images.unsplash.com/photo-1574852859542-1b41217a7815"
+                  alt="A person with headphones on drawing a product design."
+                />
+                <div className="overlay">
+                  <p>
+                    Product & <span>Branding Design</span>
+                  </p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"jobs/4"}>
+                <img
+                  src="https://images.unsplash.com/photo-1632406898177-95f7acd8854f"
+                  alt="A person sitting with their notebook open and their monthly budget open on their MacBook"
+                />
+                <div className="overlay">
+                  <p>
+                    Admin & <span>Customer Support</span>
+                  </p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"jobs/5"}>
+                <img
+                  src="https://images.unsplash.com/photo-1521898284481-a5ec348cb555"
+                  alt="A person with headphones on sitting on chair and working at their MacBook"
+                />
+                <div className="overlay">
+                  <p>Quality Assurance Analysis</p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"jobs/6"}>
+                <img
+                  src="https://images.unsplash.com/photo-1520333789090-1afc82db536a"
+                  alt="A person with glasses sitting at their computer and looking at their smartphone"
+                />
+                <div className="overlay">
+                  <p>Social Media Management</p>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to={"jobs/6"}>
+                <img
+                  src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e"
+                  alt="A person sitting at a table writing on notebook. On the table is a stack of books, a pile of polaroids, a MacBook, an iPad, a pair of glasses, and a cup of black coffee."
+                />
+                <div className="overlay">
+                  <p>Copywriting</p>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
